8b58b63f463eee87590df942164fcb64557acb49
unknown <sanmanesanjay08@gmail.com>
2023-05-24 07:32:24 +0530

    checmistry file added

--- README.md
+++
@@ -1,2 +0,0 @@
-# repofirst
-first repository for github practice sessrion
---
+++ calc.py
@@ -0,0 +1,9 @@
+a=50
+b=20
+
+c=a+b
+print("Additon of a&b\n",c)
+c=a-b
+print("Substaraciion of a & b\n",c)
+c=a*b
+print("Multiplication od a&b\n",c)
